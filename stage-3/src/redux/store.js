c9275b9fdfd3dd4bd554c732a63940bfefaa2c40
Aaron Estes <aestesc@gmail.com>
2018-10-23 15:59:38 -0500

    stage 3

--- stage-3/src/redux/store.js
+++ stage-3/src/redux/store.js
@@ -1,4 +1,9 @@
+import { createStore, applyMiddleware } from 'redux';
 import reducer from './reducer';
 import reduxPromiseMiddleware from 'redux-promise-middleware';
 
 export default createStore(reducer, applyMiddleware(reduxPromiseMiddleware()));
+
+// import { createStore } from 'redux';
+// import reducer from './ducks/reducer';
+// export default createStore( reducer );
